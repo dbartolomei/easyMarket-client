1e26b049ad948b2ec3d74692d547fa80046f5cc2
Alejandro <alejandro@alejandro.(none)>
2013-12-12 07:01:29 -0400

    stuff

--- controller/cart.js
+++ controller/cart.js
@@ -5,6 +5,37 @@ $(document).on('pagebeforeshow', "#cart", function(event, ui) {
         $.mobile.changePage("login.html",{ });
     } 
     else{
+    	
+    		$.ajax({
+            url : "http://localhost:5000/creditcard",
+            contentType: "application/json",
+            data: {user_id:localStorage.currentUser},
+            success : function(data, textStatus, jqXHR){
+           var area=$("#creditcards");
+           area.empty();
+           var jason= data.data;
+           console.log(jason);
+           
+   var cat;
+   for(var i=0; i<jason.length;i++){
+   	cat=jason[i];
+   	
+    area.append("<option value="+  cat.creditcard_id   +">"+  (cat.creditcard_number).substring(12,16)+"-"+cat.company +"</option>");
+   	
+    
+         area.fieldcontain("refresh");
+           }
+          
+       // area.append("</select>");   
+           },
+            
+            error: function(data, textStatus, jqXHR){
+                console.log("textStatus: " + textStatus);
+                alert("Data not found!");
+            }
+       });
+     
+	
 	$.ajax({
 		url : "http://easymarket.herokuapp.com/cartproducts",
 		contentType: "application/json",
@@ -23,7 +54,7 @@ $(document).on('pagebeforeshow', "#cart", function(event, ui) {
 				
 				
 				product = cartList[i];
-				list3.append("<li id='listitem'><a onclick=GetProductPage(" + product.product_id + ")>"
+				list3.append("<li id=" + product.product_id + "><a onclick=GetProductPage(" + product.product_id + ")>"
 								+ "<h2>" + product.product_name +  "</h2>" 
 								+ "<p>" + product.description + "</p>" 
 								+ "<p>Price: " + product.price + "</p>" +"</a></li>"
@@ -34,16 +65,67 @@ $(document).on('pagebeforeshow', "#cart", function(event, ui) {
 			}
 			total.append("<h4 style='text-align: right'>Total: $ "+product.price_total +"</h4>");
 		$("li").on("swipe",function(){
+           
             $(this).hide();
             $("h4").hide();
+            var data = {
+		user_id:localStorage.currentUser
+		//product_id: $(this)
+		};
+alert(data[0]);
+	// var url = "http://localhost:5000/remove";
+// 
+// 	
+		// $.post(url, data, function(){
+			// console.log('sucess');
+		// })
+		// .done(function(){
+			// alert("Chech your email for receipt!");
+			// $.mobile.changePage( "account.html", { reloadPage: true, transition: "none"} );
+// 			
+		// })
+		// .fail(function(){
+			// alert('System Error: Please try later');
+		// })
+// 	
   });    
 		},
 		error: function(data, textStatus, jqXHR){
 			console.log("textStatus: " + textStatus);
 			alert("Data not found!");
 		}
-	});}
+	});
+	
+      
+	
+	
+	
+	}
 });
 
+function checkout(){
+
+		var data = {
+		user_id:localStorage.currentUser};
+
+	var url = "http://localhost:5000/checkout";
+
+	
+		$.post(url, data, function(){
+			console.log('sucess');
+		})
+		.done(function(){
+			alert("Chech your email for receipt!");
+			$.mobile.changePage( "account.html", { reloadPage: true, transition: "none"} );
+			
+		})
+		.fail(function(){
+			alert('System Error: Please try later');
+		})
+	
+	
+	
+	
+}
 
 
---
+++ controller/new_product.js
@@ -0,0 +1,88 @@
+$(document).on('ready',function(event){  
+
+$('#pricespace').hide();
+$('#auctionspace').hide();
+
+});
+
+$(document).on('pagebeforeshow', "#sell", function(event, ui) {
+      
+		$.ajax({
+            url : "http://localhost:5000/categoriesfornewproduct",
+            contentType: "application/json",
+            success : function(data, textStatus, jqXHR){
+           var area=$("#categories");
+           area.empty();
+           var jason= data.data;
+         
+           
+   var cat;
+   for(var i=0; i<jason.length;i++){
+   	cat=jason[i];
+   	if(cat.child_id==null){
+    area.append("<option value="+  cat.category_id   +">"+  cat.parent_name+"-"+cat.category_name  +"</option>");
+     }
+     else{
+     area.append("<option value="+  cat.child_id   +">"+  cat.parent_name+"-"+cat.category_name +"-"+cat.child_name  +"</option>");	
+     	
+     }
+         area.fieldcontain("refresh");
+           }
+        area.append("</select>");   
+           },
+            
+            error: function(data, textStatus, jqXHR){
+                console.log("textStatus: " + textStatus);
+                alert("Data not found!");
+            }
+       });
+      
+});
+function priceshow(){
+	
+	$('#pricespace').show();
+	
+	
+}
+function auctionshow(){
+	$('#auctionspace').show();
+	
+	
+}
+
+
+function new_product(){
+	
+	var data = {
+		name : $('#name').val(),
+		description : $('#description').val(),
+		model : $('#model').val(),
+		image: $('#image').val(),
+		brand: $('#brand').val(),
+		dimensions : $('#dimensions').val(),
+		category : $('#categories').val(),
+		sellprice : $('#sellprice').val(),
+		end_time : $('#end_time').val(),
+		stock : $('#stock').val(),
+		auctionprice: $('#auctionprice').val(),
+		user_id:localStorage.currentUser
+		//local storage
+	};
+
+	var url = "http://localhost:5000/new_product";
+
+	
+		$.post(url, data, function(){
+			console.log('sucess');
+		})
+		.done(function(){
+			alert("Product added!");
+			$.mobile.changePage( "sell.html", { reloadPage: true, transition: "none"} );
+			//$.mobile.changePage( "account.html", { transition: "slideup", changeHash: false });
+			//change location to addresses view
+		})
+		.fail(function(){
+			alert('System Error: Please try later');
+		})
+	
+}
